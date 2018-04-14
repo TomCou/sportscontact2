72485460bf4644ecff470aad8c19ef383afa46d3
TomCou <tommy.cousineau.tc@gmail.com>
2018-04-14 14:50:55 -0500

    this is a commitment

---
+++ cmdForPush.bash
@@ -0,0 +1,16 @@
+
+
+
+
+# commande for pushing changes
+git add *
+
+pause
+
+git commit -m "this is a commitment"
+
+pause
+
+git push origin master
+
+pause
---
+++ setupPY/venv/src/setupInv.py
@@ -0,0 +1,98 @@
+
+#!/usr/bin/python
+
+
+import pip
+pip.main(['install', 'schedule'])
+pip.main(['install', 'gitpython'])
+import os
+import tkinter
+from tkinter import *
+from inspect import currentframe, getframeinfo
+import shutil
+import os
+import glob
+import datetime
+import threading
+import schedule
+import time
+from os import listdir
+from os.path import isfile, join
+import json
+from pprint import pprint
+from shutil import *
+#import git
+
+
+#rw_dir='C:/Users/developmentPC/Documents/dev/sportscontact/'
+#repo = Repo(rw_dir)
+
+def job(t):
+    shutil.copy2('//SCONTACTSRV/Public/Commun/Export inv rpp/exportinvtxt.txt','C:/Users/developmentPC/Documents/dev/sportscontact/dbPre.json')
+    shutil.copy2('//SCONTACTSRV/Public/Commun/Export inv rpp/exportinvtxt.txt','C:/Users/developmentPC/Documents/dev/sportscontact/exportinvtxt.txt')
+    aList = [];
+    finalDict = {'Items':aList};
+    checkSizeList = [];
+    checkSizeDict = {'Items':checkSizeList};
+    if(os.path.isfile('C:/Users/developmentPC/Documents/dev/sportscontact/dbW.json')):
+        os.remove('C:/Users/developmentPC/Documents/dev/sportscontact/dbW.json')
+    #data = json.load(open('dbPre.json'))
+    with open('C:/Users/developmentPC/Documents/dev/sportscontact/dbW.json', 'w') as src_file:
+        with open('C:/Users/developmentPC/Documents/dev/sportscontact/dbPre.json', 'r') as data_file:
+            data = json.load(data_file)
+            for theList, product in data.items(): #got the single list of items
+                ind = -1
+                for art in data[theList]: #each item in the list is a dict
+                    ind=ind+1
+                    if((art['qty_hb'] > 0) or (art['qty_sn'] > 0) ):
+                        #print(art['cdp'])
+                        art['cdp']= art['cdp'].rstrip()#cleanAtt(art['cdp'])
+                        art['id']=art['ID']
+                        del art['ID']
+                        print(ind,'- ', art['cdp'],'is', art['id'],'[ch#',art['qty_hb'],'][sn#',art['qty_sn'],']')
+                        finalDict['Items'].append(art)
+                        if(art['size'] is not ''):
+                            checkSizeDict['Items'].append(art)
+            json.dump(finalDict,src_file)
+
+
+    if(os.path.isfile('C:/Users/developmentPC/Documents/dev/sportscontact/db.json')):
+        os.remove('C:/Users/developmentPC/Documents/dev/sportscontact/db.json')
+
+    shutil.copy2('C:/Users/developmentPC/Documents/dev/sportscontact/dbW.json','C:/Users/developmentPC/Documents/dev/sportscontact/db.json')
+
+    with open('C:/Users/developmentPC/Documents/dev/sportscontact/db.json', 'r') as data_file:
+        data = json.load(data_file)
+    # repo.git.commit("commit time: "+time.localtime(secs))
+    # origin = repo.remote(name='origin')
+    # origin.push()
+
+
+def cleanAtt(att):
+    final = '';
+    for x in len(att):
+        y=x;
+        while(att[y] is ' '):
+            y=y+1
+            if (x - y) is 2 :
+                final=split(att,x)
+
+
+schedule.every().day.at("09:00").do(job, 'It is 09:00')
+schedule.every().day.at("10:00").do(job, 'It is 10:00')
+schedule.every().day.at("11:00").do(job, 'It is 11:00')
+schedule.every().day.at("12:00").do(job, 'It is 12:00')
+schedule.every().day.at("13:00").do(job, 'It is 13:00')
+schedule.every().day.at("14:00").do(job, 'It is 14:00')
+schedule.every().day.at("15:00").do(job, 'It is 15:00')
+schedule.every().day.at("16:00").do(job, 'It is 16:00')
+schedule.every().day.at("17:00").do(job, 'It is 17:00')
+schedule.every().day.at("18:00").do(job, 'It is 18:00')
+schedule.every().day.at("19:00").do(job, 'It is 19:00')
+schedule.every().day.at("20:00").do(job, 'It is 20:00')
+schedule.every().day.at("21:00").do(job, 'It is 21:00')
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # wait one minute
+
