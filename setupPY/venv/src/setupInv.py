4c45ae07d496b3848755c1f4d45e06c1eafd8095
TomCou <tommy.cousineau.tc@gmail.com>
2018-04-14 16:03:44 -0500

    this is a commitment

--- setupPY/venv/src/setupInv.py
+++ setupPY/venv/src/setupInv.py
@@ -56,9 +56,9 @@ def job(t):
                         finalDict['Items'].append(art)
                         if(art['size'] is not ''):
                             checkSizeDict['Items'].append(art)
-                        if ind == 30:
+                        if ind == 29:
                             break
-                if ind == 30:
+                if ind == 29:
                     break
             json.dump(finalDict,src_file)
 
