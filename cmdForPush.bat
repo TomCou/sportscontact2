558c934fcd850207faffadd524992a1b7be47fca
TomCou <tommy.cousineau.tc@gmail.com>
2018-04-15 12:52:09 -0500

    this is a commitment

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -2,11 +2,13 @@
 
 
 
-# commande for pushing changes
+#commande for pushing changes
 git add --all
 
 git commit -m "this is a commitment"
 
 git push https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git master
+
+git push heroku master
 
 pause
