e404c305be9ffb326951bac4718aef0549e97458
TomCou <tommy.cousineau.tc@gmail.com>
2018-04-15 12:02:48 -0500

    this is a commitment

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -7,6 +7,6 @@ git add --all
 
 git commit -m "this is a commitment"
 
-git push https://TomCou:dtw4571s@sportscontact.biz/ master
+git push https://TomCou:dtw4571s@github.com/TomCou/sportscontact.git master
 
 pause
