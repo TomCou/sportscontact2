1156cc440bcb4816140a369eef3c7d4427f64a19
TomCou <tommy.cousineau.tc@gmail.com>
2018-05-21 11:04:32 -0500

    this is a commitment

--- setupPY/venv/src/ui.py
+++ setupPY/venv/src/ui.py
@@ -419,8 +419,8 @@ class RWHANDLE(object):
             prevSheet.title = currSheetTitle+'_old'
             #self.wb.create_sheet(index=0, title=currSheetTitle)
             #newSheet = self.wb[currSheetTitle]#.get_sheet_by_name(currSheetTitle)
-            newSheetName=self.wb.copy_worksheet(self.wb.get_sheet_by_name('TEMPLATE'))
-            newSheet=self.wb[newSheetName]
+            
+            newSheet=self.wb.copy_worksheet(self.wb.get_sheet_by_name('TEMPLATE'))
             newSheet.title(currSheetTitle)
             prevSheet = self.wb[currSheetTitle+'_old']#that verifies that it is in the wb
 
