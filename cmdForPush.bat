d10bdf61ac27c67e0e32b80d57d28f1d3deed8e4
TomCou <tommy.cousineau.tc@gmail.com>
2018-05-21 10:21:05 -0500

    this is a commitment

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -4,7 +4,7 @@
 
 #commande for pushing changes
 
-git pull https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git
+#git pull https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git
 
 #72ce4ac97d95f4c1cd9050061b657c71ae644fc9
 git add --all
@@ -16,4 +16,4 @@ git push https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git master
 
 git push heroku master
 
-pause
+#pause
