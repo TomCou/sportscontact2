85c2ee8e4d3f468a3e160c02119d445bb96b885b
TomCou <tommy.cousineau.tc@gmail.com>
2018-09-24 16:30:42 -0400

    this is a commitment - 24/09/2018_16:30:42.48

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -4,6 +4,8 @@
 
 #commande for pushing changes
 
+git commit -m "this is a commitment - %_my_datetime%"
+
 git pull https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git
 
 #72ce4ac97d95f4c1cd9050061b657c71ae644fc9
