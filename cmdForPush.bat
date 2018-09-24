2837392c540d8aa82dd3fa9e091b7823a5fdf77b
TomCou <tommy.cousineau.tc@gmail.com>
2018-09-24 16:27:30 -0400

    this is a commitment - 24/09/2018_16:27:30.48

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -9,7 +9,9 @@ git pull https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git
 #72ce4ac97d95f4c1cd9050061b657c71ae644fc9
 git add --all
 
-git commit -m "this is a commitment - `date '+%Y-%m-%d %H:%M:%S'`"
+set _my_datetime=%date%_%time%
+
+git commit -m "this is a commitment - %_my_datetime%"
 
 git push https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git master
 
