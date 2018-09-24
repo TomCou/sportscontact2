bc0924630f935b8819c3db3c97f4d94287ee1040
TomCou <tommy.cousineau.tc@gmail.com>
2018-09-24 16:23:48 -0400

    this is a commitment - `date '+m-H:S'`

--- cmdForPush.bat
+++ cmdForPush.bat
@@ -9,9 +9,15 @@ git pull https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git
 #72ce4ac97d95f4c1cd9050061b657c71ae644fc9
 git add --all
 
-git commit -m "this is a commitment"
+pause
+
+git commit -m "this is a commitment - `date '+%Y-%m-%d %H:%M:%S'`"
+
+pause
 
 git push https://TomCou:dtw4571s@github.com/TomCou/sportscontact2.git master
+
+pause
 
 git push heroku master
 
